c75cfbb839bd649a13901c0aaa278e26164d5a56
EnMar32 <enachescu.maria@gmail.com>
2022-09-22 18:05:15 +0300

    changed default gameTime for test env

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -8,7 +8,7 @@ export const environment = {
   availableLanguages: ['da-DK', 'nn-NO', 'sv-SE', 'nb-NO'],
   logLevel: NgxLoggerLevel.OFF,
   location: 'https://10finger-test.intowords.com/',
-  gameTime: '00:20',
+  gameTime: '02:00',
   UrlEndpoints: {
     auth: 'https://signon-test.vitec-mv.com',
     voiceservice: 'https://voiceservice-test.vitec-mv.com',
