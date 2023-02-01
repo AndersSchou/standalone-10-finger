e3262fe4daa8fbd93de9e59ed0fd73767988510c
EnMar32 <enachescu.maria@gmail.com>
2023-02-01 13:45:31 +0200

    fixed start now button

--- src/app/modules/games/fishing/instructions/instructions.component.spec.ts
+++ src/app/modules/games/fishing/instructions/instructions.component.spec.ts
@@ -4,32 +4,20 @@ import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
 import { MaterialModule } from 'src/app/modules/shared/material.module';
-import { LanguageHelperService } from 'src/app/services/language.service';
 import { AppGamesFishInstructionsComponent } from './instructions.component';
 
 describe('AppGamesFishInstructionsComponent', () => {
   let component: AppGamesFishInstructionsComponent;
   let fixture: ComponentFixture<AppGamesFishInstructionsComponent>;
-  let languageHelperServiceSpy: jasmine.SpyObj<LanguageHelperService>;
-  let onLanguageChangedSpy;
   let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    languageHelperServiceSpy = jasmine.createSpyObj<LanguageHelperService>(['OnLanguageChanged']);
-    (languageHelperServiceSpy as any).OnLanguageChanged = new Observable(
-      (subscriber) => {
-        onLanguageChangedSpy = subscriber;
-      }
-    );
-
     routerSpy = jasmine.createSpyObj<Router>(['navigate']);
 
     await TestBed.configureTestingModule({
       declarations: [AppGamesFishInstructionsComponent],
       providers: [
-        { provide: LanguageHelperService, useValue: languageHelperServiceSpy },
         { provide: Router, useValue: routerSpy },
       ],
       imports: [
