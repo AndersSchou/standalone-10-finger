39e16e90500329a30b59ff6c6ea84dc1cab9ad52
EnMar32 <enachescu.maria@gmail.com>
2023-05-08 12:15:31 +0300

    added orange color to shift keys

--- src/app/vkeyboard/vkeyboard.component.ts
+++ src/app/vkeyboard/vkeyboard.component.ts
@@ -159,12 +159,13 @@ export class VKeyboardComponent {
    * @returns The shift class name.
    */
   getShiftClass(row: number, col: number): string {
+    console.log('row', row, 'col', col);
     if (((row === 1 || row === 4) && col > 7) || ((row === 2 || row === 3) && col > 6)) {
       // Right shift.
-      return '4-1';
+      return '4-13';
     } else {
       // Left shift.
-      return '4-13';
+      return '4-1';
     }
   }
 
