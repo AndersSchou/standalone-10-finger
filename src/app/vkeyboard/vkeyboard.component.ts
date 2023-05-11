c82c25fc97738b5844890ce2712d0260516e8e1a
EnMar32 <enachescu.maria@gmail.com>
2023-05-11 15:22:39 +0300

    switched shift keys positions

--- src/app/vkeyboard/vkeyboard.component.ts
+++ src/app/vkeyboard/vkeyboard.component.ts
@@ -160,11 +160,11 @@ export class VKeyboardComponent {
    */
   getShiftClass(row: number, col: number): string {
     if (((row === 1 || row === 4) && col > 7) || ((row === 2 || row === 3) && col > 6)) {
-      // Right shift.
-      return '4-13';
-    } else {
       // Left shift.
       return '4-1';
+    } else {
+      // Right shift.
+      return '4-13';
     }
   }
 
